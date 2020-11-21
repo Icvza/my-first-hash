9dfe23ffccb03a021fd117fec6b687b5bd3540f2
Isaac Chavez <devisaacalejandro@gmail.com>
2020-11-21 16:22:36 -0500

    commit tree 0a53e1ced1b554a51d4767ce2a55809e79cb0602

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,11 +1,15 @@
 def my_hash
-  # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
+
+  your_hash = { "My_name" => "Isaac Alejandro Chvaez", "My_age" => "23, Im getting old", "My_location" => "New Jersy" }
 
   
 end
 
 
 def shipping_manifest
+
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
@@ -21,8 +25,10 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
+
+
 
 def adding
   shipping_manifest = {
@@ -32,12 +38,14 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 
-
+                 shipping_manifest["muskets"] = 2
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
+                 shipping_manifest["gun powder"] = 4
 
   # return the shipping_manifest hash below
+
+  return shipping_manifest
 
 end
